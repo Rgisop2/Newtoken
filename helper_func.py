339cc82f0cf47986163942f5bdec7a04c3e33fea
Rgisop2 <160823869+Rgisop2@users.noreply.github.com>
2025-12-02 16:45:52 +0530

    Update helper_func.py

--- helper_func.py
+++ helper_func.py
@@ -103,7 +103,7 @@ async def get_verify_status(user_id):
 
 async def update_verify_status(user_id, verify_token="", is_verified=False, verified_time=0, link="", 
                               current_step=None, verify1_expiry=None, verify2_expiry=None, gap_expiry=None,
-                              verification_message_id=None):
+                              last_verification_message_id=None):
     current = await db_verify_status(user_id)
     current['verify_token'] = verify_token
     current['is_verified'] = is_verified
@@ -119,9 +119,8 @@ async def update_verify_status(user_id, verify_token="", is_verified=False, veri
         current['verify2_expiry'] = verify2_expiry
     if gap_expiry is not None:
         current['gap_expiry'] = gap_expiry
-    
-    if verification_message_id is not None:
-        current['verification_message_id'] = verification_message_id
+    if last_verification_message_id is not None:
+        current['last_verification_message_id'] = last_verification_message_id
     
     await db_update_verify_status(user_id, current)
 
